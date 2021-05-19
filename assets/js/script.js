628bade244608135c8423687919709cd25650563
Jamie Vesterfelt <jamievesterfelt@hotmail.com>
2021-05-19 12:11:47 -0600

    Started writing script.js file.

---
+++ assets/js/script.js
@@ -0,0 +1,40 @@
+var startBtn = document.querySelector("#start");
+var btnA = document.querySelector("#a");
+var btnB = document.querySelector("#b");
+var btnC = document.querySelector("#c");
+var btnD = document.querySelector("#d");
+var questionNumber = 0;
+var questions = [{
+        q: "Array content is placed within ____.",
+        a: "brackets"
+    },
+    {
+        q: "Object elements are placed within ____.",
+        a: "curly brackets"
+    },
+    {
+        q: "Elements of arrays and objects use a ____ separator.",
+        a: "comma"
+    },
+    {
+        q: "Clicking a button represents an ____.",
+        a: "event"
+    },
+    {
+        q: "Javascript uses a ____ as a statement terminator.",
+        a: "semicolon"
+    }
+];
+
+
+var startQuiz = function() {
+
+};
+
+function quiz() {
+    if (questionNumber < 1) {
+
+    }
+};
+
+quiz();
